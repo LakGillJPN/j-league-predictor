46fa6e1118552e3d26f20b978aa336aa2b116d25
LakGillJPN <lakgill88@gmail.com>
2023-04-12 19:28:47 +0900

    feat: add logos to play page

--- frontend/src/components/FixturesCarousel.jsx
+++ frontend/src/components/FixturesCarousel.jsx
@@ -2,7 +2,7 @@ import {React, useEffect, useState} from 'react';
 import { Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./FixturesCarousel.css";
-import axios from 'axios';
+import getFixtures from '../utils/get-fixtures'; // import the fixture list
 
 export default function FixturesCarousel() {
   const [fixtures, setFixtures] = useState([]);
@@ -11,10 +11,7 @@ export default function FixturesCarousel() {
     getFixtures()
   },[])
 
-  async function getFixtures() {
-    const fetchedFixs = await axios.get('/fixtures');
-    setFixtures(fetchedFixs.data.filter(x => x.gameweek === 'Regular Season - 8'))
-  }
+  getFixtures(setFixtures); // set the fixtures list
 
   return (
     <>
@@ -28,14 +25,17 @@ export default function FixturesCarousel() {
   
    {fixtures.map((fixture) => (
     <div key={fixture.id}>
-    <div className='images'>
-    <div className='home-logo'><img src={fixture.home_team_logo} alt=""/></div>
-    <div className='away-logo'><img src={fixture.away_team_logo} alt=""/> </div>
+
+
+
+    {/* <div className='images'>
+      <div className='home-logo'><img src={fixture.home_team_logo} alt=""/></div>
+      <div className='away-logo'><img src={fixture.away_team_logo} alt=""/> </div>
     </div>
     <p className='game'>{`${fixture.home_team} vs ${fixture.away_team}`} </p>
     <p className='date'>{` ${new Date(fixture.date).toDateString()}`} </p>
-    <p className='time'> {` ${new Date(fixture.date).toTimeString()}`}  </p>
-    
+    <p className='time'> {` ${new Date(fixture.date).toTimeString()}`}  </p>  */}
+   
   </div>
 ))}
     </Carousel>
@@ -46,3 +46,6 @@ export default function FixturesCarousel() {
   </>
   )
 }
+
+/*
+   
--- frontend/src/pages/Play.js
+++ frontend/src/pages/Play.js
@@ -1,7 +1,8 @@
 import {useState, useEffect,React} from 'react';
-import axios from 'axios';
 import Header from '../components/Header';
+import './Play.css'
 import getFixtures from '../utils/get-fixtures';
+
 
 export default function Play() {
   const [fixtures, setFixtures] = useState([]);
@@ -15,20 +16,31 @@ export default function Play() {
   return (
     <>
     <Header/>
-    <h1>Make your Predicatons</h1>
+    <h1>Make your Predications</h1>
 
     {fixtures.map((fixture) => (
+    
     <div key={fixture.id}>
-    <div className='images'>
-    <div className='home-logo'><img src={fixture.home_team_logo} alt=""/></div>
-    <div className='away-logo'><img src={fixture.away_team_logo} alt=""/> </div>
+   
+    <div className='predict-game'>  
+    <div className='game-box'>
+      <div> <img className='logo' src={fixture.home_team_logo} />  </div>
+       {fixture.home_team} 
+       <div><input type="number" className="scorebox"  pattern="[0-9.]" min="0" max="10" required></input></div>
     </div>
-    <p className='game'>{`${fixture.home_team} vs ${fixture.away_team}`} </p>
-    <p className='date'>{` ${new Date(fixture.date).toDateString()}`} </p>
-    <p className='time'> {` ${new Date(fixture.date).toTimeString()}`}  </p>
+
+    <div className='game-box'>
+    <div> <img className='logo' src={fixture.away_team_logo} /> </div>
+       {fixture.away_team}
+       <div><input type="number" className="scorebox" required></input></div>
+    </div>
+
+      </div>
+      <div className="space"></div>
+    
     
   </div>
-   ))}
+))}
 
   
 

78e3f68f704eb3dcb4ebd62f0473974ab2fe2c56
LakGillJPN <lakgill88@gmail.com>
2023-04-16 23:15:52 +0900

    fix: changed the background and add a header

---
+++ frontend/src/utils/get-gameweek.js
@@ -0,0 +1,10 @@
+const getGameweek = (gameweek) => {
+  if (isNaN(parseInt(gameweek[gameweek.length-2]))) {
+    return gameweek[gameweek.length-1]
+  }
+  else {
+    return gameweek[gameweek.length-2] + gameweek[gameweek.length-1]
+  }
+};
+
+export default getGameweek;

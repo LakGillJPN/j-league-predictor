206b4d3fe6188cefd451b544c61361ec12e09804
LakGillJPN <lakgill88@gmail.com>
2023-04-17 23:17:00 +0900

    feat: make call to backend to get results data

--- frontend/src/pages/Results.js
+++ frontend/src/pages/Results.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import Header from '../components/Header';
+import getResults from '../utils/get-results';
+import { UserAuth } from '../context/AuthContext';
 
 export default function Results() {
+
+  let {userEmail} = UserAuth();
+  let [results, setResults] = useState([]);
+
+  useEffect(() => {
+    console.log(results)
+  },[results]);
+
+  getResults(setResults,userEmail);
+
   return (
     <>
     <Header/>
@@ -9,6 +21,8 @@ export default function Results() {
     <p>Correct Home or Away Score - 20PTs</p>
     <p>Correct Winner + Correct Home or Away Score - 70pts</p>
     <p>Correct Winner + Correct Score - 100pts</p>
+
+
     </>
   )
 }

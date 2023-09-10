019fe35797ab83b8ab604b1ff9be8fda9a726bed
LakGillJPN <lakgill88@gmail.com>
2023-09-10 23:06:15 +0900

    feat: write test to check email label

--- frontend/src/pages/tests/Login.test.tsx
+++ frontend/src/pages/tests/Login.test.tsx
@@ -1,17 +1,24 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
 import { render } from '@testing-library/react';
-import Login from '../Login'
-import '@testing-library/jest-dom'
+import Login from '../Login';
+import '@testing-library/jest-dom';
+import { AuthContextProvider } from '../../context/AuthContext';
+import { HashRouter } from 'react-router-dom';
 
-test('renders Login component', () => {
-  const { getByText, getByLabelText } = render(<Login />);
-  
-  // Check if important elements are present
-  const emailInput = getByLabelText('Email:');
-  const passwordInput = getByLabelText('Password:');
-  const loginButton = getByText('Login');
-  
-  expect(emailInput).toBeInTheDocument();
-  expect(passwordInput).toBeInTheDocument();
-  expect(loginButton).toBeInTheDocument();
+test('renders Email label in Login component', () => {
+  const { getByLabelText } = render(
+    <HashRouter>
+      <AuthContextProvider>
+        <Login />
+      </AuthContextProvider>
+    </HashRouter>
+  );
+
+  // Check if the "Email:" label is present
+  const emailLabel = getByLabelText('Email:');
+  expect(emailLabel).toBeInTheDocument();
 });
--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['dotenv/config'], // Load dotenv for all tests
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy', // Replace with the correct path
+  },
 };

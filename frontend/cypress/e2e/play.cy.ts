873ef08c9f738c14a7f58c3b498faca09d4f704e
LakGillJPN <lakgill88@gmail.com>
2023-09-18 20:43:52 +0900

    feat: add logout Cypress test

--- frontend/cypress/e2e/play.cy.ts
+++ frontend/cypress/e2e/play.cy.ts
@@ -1,10 +1,20 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Play', () => {
+
+  beforeEach(() => {
     cy.visit('/#/login')
     cy.get('#email').type(Cypress.env("username"));
     cy.get('[data-testid="password-input"]').type(Cypress.env("password"));
     cy.get('[data-testid="login-button"]').click();
     cy.visit('/#play')
   })
+
+  it('Should enter data into checkboxes', () => {
+    cy.get('.scorebox').each(($input) => {
+      // Ensure the element is enabled (remove 'disabled' attribute)
+     // Need to make a Play-Test page.
+  });
+})
+
+
 })
 

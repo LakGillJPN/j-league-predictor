7b2e88776317bed0d1014952d7849021a594a78d
LakGillJPN <lakgill88@gmail.com>
2023-09-24 22:27:01 +0900

    feat: add play-test page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp.tsx';
 import Results from './pages/Results.tsx';
 import Submitted from './pages/Submitted.tsx';
 import Footer from './components/Footer.tsx'
+import PlayTest from './pages/Play-Test.tsx';
 import {Routes, Route, HashRouter} from "react-router-dom";
 import { AuthContextProvider } from './context/AuthContext.tsx';
 import ProtectedRoute from './context/ProtectedRoute.tsx';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/play/" element ={<ProtectedRoute> <Play/> </ProtectedRoute>} />
           <Route path="/submitted/" element={<ProtectedRoute> <Submitted/> </ProtectedRoute>} />
           <Route path="/results/" element={<ProtectedRoute> <Results/> </ProtectedRoute>} />
+          <Route path="/play-test/" element ={<ProtectedRoute> <Play/> </ProtectedRoute>} />
         </Routes>
         </HashRouter>
         </AuthContextProvider>
---
+++ frontend/src/pages/Play-Test.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+import Header from '../components/Header.tsx';
+import './Play.css'
+import getFixtures from '../utils/get-fixtures.ts';
+import { UserAuth,} from '../context/AuthContext.tsx';
+import axios from 'axios';
+import Warning from '../components/Warning.tsx';
+import { useNavigate } from 'react-router-dom';
+import { playGameweek } from '../utils/get-gameweek.ts';
+//import CountdownTimer from '../components/CountdownTimer.jsx';
+import { Fixture } from "../../../globals";
+
+export default function PlayTest() {
+  const [fixtures, setFixtures] = useState([]);
+  const [homePredications, setHomePredications] = useState([]);
+  const [awayPredications, setAwayPredications] = useState([]);
+  const [gameweek, setGameweek] = useState<string[]>([]);
+  const {userEmail, userPredications, uid} = UserAuth();
+
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    getFixtures(setFixtures) 
+  },[])
+
+  useEffect( () => {
+    playGameweek(setGameweek)
+  },[])
+
+
+  const handleHomeChange = (event: { target: { name: string; value: string; }; }) => {
+    const { name, value } = event.target;
+    setHomePredications(prevState => ({
+      ...prevState,
+      [name]: parseInt(value)
+    }));
+  }
+
+  const handleAwayChange = (event: { target: { name: string; value: string; }; }) => {
+    const { name, value } = event.target;
+    setAwayPredications(prevState => ({
+      ...prevState,
+      [name]: parseInt(value)
+    }));
+  }
+
+  const predictArr = (home: object, away: object) => {
+    const id : string[] = Object.keys(home);
+    const homeScore : number[] = Object.values(home);
+    const awayScore : number[] = Object.values(away);
+
+    let newArr = [];
+
+    for (let i = 0; i < id.length; i++) {
+      newArr.push([id[i],homeScore[i],awayScore[i]])
+    }
+    return newArr
+  }
+
+  const handleFormSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    const result = predictArr(homePredications, awayPredications);
+    axios.post('/api/predications', {
+      uid,
+      predications: result,
+      current_gameweek: gameweek
+    })
+    .then(response => {
+      navigate('/submitted')
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+ 
+
+  return (
+    <>
+    <Header/>
+    {/* <h1>You've got until:</h1>
+      <CountdownTimer deadline={Date(deadline)} /> */}
+    <h1 className='play-header'>Make Your Predications!</h1>
+
+    
+
+    {userPredications.length > 1 ? <Warning/> : <> 
+
+    <form onSubmit={handleFormSubmit}> 
+
+    {fixtures.map((fixture: Fixture) => (
+    
+      <div className='fixtures-box' key={fixture.fixture_id}>
+   
+        <div className='predict-game'>  
+          <div className='game-box'>
+            <div> <img className='logo' src={fixture.home_team_logo_url} />  </div>
+            <div> {fixture.home_team_name} </div>
+            </div>
+
+        <div className='scorebox-container'>
+          <input type="text" className="scorebox" name={`${fixture.fixture_id}`} 
+            maxLength={1} pattern="[0-9.]" min="0" max="10" 
+            onChange={handleHomeChange} required></input>
+            
+        <div className="colon"></div>
+  
+          <input type="text" className="scorebox" name={`${fixture.fixture_id}`} 
+            maxLength={1} pattern="[0-9.]" min="0" max="10" 
+            onChange={handleAwayChange} required></input> 
+        </div>
+
+        <div className='game-box'>
+          <div> <img className='logo' src={fixture.away_team_logo_url} /> </div>
+          <div> {fixture.away_team_name} </div>
+        </div>
+        {/* <div className='date-and-time'>
+        <div className='date-and-time'>{`${new Date(fixture.date).toDateString()}`} </div>
+        <div className='date-and-time'>{`${new Date(fixture.date).toTimeString()}`} </div> 
+        </div> */}
+      </div>
+      
+
+      <div className="space"></div>  
+    </div>
+  ))}
+
+    <button type='submit'>SUBMIT</button>
+  </form>
+  </>}
+</>
+ )
+}

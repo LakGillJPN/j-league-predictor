c8fe69d599616709a96bf6972c15a8c854ccb516
LakGillJPN <lakgill88@gmail.com>
2023-09-26 13:23:10 +0900

    feat: add Entry Form page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import Play from './pages/Play.tsx';
 import SignUp from './pages/SignUp.tsx';
 import Results from './pages/Results.tsx';
 import Submitted from './pages/Submitted.tsx';
+import EntryForm from './pages/EntryForm.tsx';
+import Header from './components/Header.tsx';
 import Footer from './components/Footer.tsx'
 import {Routes, Route, HashRouter} from "react-router-dom";
 import { AuthContextProvider } from './context/AuthContext.tsx';
@@ -21,6 +23,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/login/" element={<Login/>} />
           <Route path="/signup/" element={<SignUp/>} />
+          <Route path ="/entryform/" element = {<EntryForm/>} />
           <Route path="/play/" element ={<ProtectedRoute> <Play/> </ProtectedRoute>} />
           <Route path="/submitted/" element={<ProtectedRoute> <Submitted/> </ProtectedRoute>} />
           <Route path="/results/" element={<ProtectedRoute> <Results/> </ProtectedRoute>} />
---
+++ frontend/src/pages/EntryForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header.tsx'
+
+export default function EntryForm() {
+  return (
+    <> 
+    <Header/>
+    <h1>EntryForm</h1>
+    
+    </>
+  )
+}

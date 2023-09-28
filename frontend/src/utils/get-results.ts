8439dd09d852d07411075159f59a4943a1f714bf
LakGillJPN <lakgill88@gmail.com>
2023-09-28 21:14:43 +0900

    revert gameweek back to current week

--- frontend/src/utils/get-results.ts
+++ frontend/src/utils/get-results.ts
@@ -8,8 +8,8 @@ async function getResults (setter: (arg0: any) => void, uid: string) {
     const resultsWeek = fetchedFixs.data.filter((data: Fixture) => data.isFinished === 'FT')
     console.log('RESULTS', fetchedResults)
     setter(fetchedResults.data.filter((data: Predication) => 
-      data.gameweek === 'Regular Season - 18'
-      //data.gameweek === resultsWeek[resultsWeek.length-1].gameweek 
+      //data.gameweek === 'Regular Season - 18' - FOR TESTING PURPOSES
+      data.gameweek === resultsWeek[resultsWeek.length-1].gameweek 
       && data.uid === uid
     ));
   }

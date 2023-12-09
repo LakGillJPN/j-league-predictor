092ef98185901bb78d23c7dafdba9ceea85ea497
Lak Gill <LakGill88@gmail.com>
2023-12-09 21:19:42 +0900

    79 decide on the apps state inbetween seasons (#80)
    * refactor: change Gameweek to Week 1
    
    * refactor: change results and totals to Gameweek 1
    
    * feat: add notice explaning the situation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,9 +9,16 @@ export default function Home(): JSX.Element {
       <Header/>  
       <div className="carousel-container">
         <FixturesCarousel/> 
-        </div>
+      </div>
+      
+      <div className='notice-container'>
+        <h1 className='notice'>Notice</h1>
+        <p className='season-notice'> The J-League 2023 Season has ended. <br/> For testing purposes, the date has been reset to Gameweek 1. </p>
+        <p className='season-notice'> Jリーグ2023シーズンが終了しました。 <br/>  テストのため、日付は Gameweek 1 にリセットされています。 </p>
+        <p className='season-notice-end'>See you next season!</p>
+      </div>
     </div>
   );
-}
+} 
 
 
